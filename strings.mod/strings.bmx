2882f91db0a483682f40be8fa21430051f1a4745
Sophie Kirschner <sophie@thehumangeo.com>
2015-05-14 06:55:40 -0400

    Added several new functions to strings.mod

--- strings.mod/strings.bmx
+++ strings.mod/strings.bmx
@@ -4,44 +4,104 @@ module mach.strings
 moduleinfo "License: zlib/libpng"
 moduleinfo "Author: Sophie Kirschner (sophiek@pineapplemachine.com)"
 moduleinfo "11 May 2015: Added to mach.mod"
+moduleinfo "14 May 2015: Added functions concat, interleave, and format"
 
 import mach.enumeration
 
 import "strings.c"
 private
 extern "c"
-    function bbStringJoinSub$(delim$, bits$[], low%, up%)
-    function bbStringJoin$(delim$, bits$[]) ' from blitz_string.c
+    function machStringFormat$(str$, bits$[], tokenchar@@)
+    function machStringJoinSub$(delim$, bits$[], low%, up%)
+    function machStringConcat$(bits$[], low%, up%)
+    function machStringInterleave$(bits$[][], start%, inc%)
 end extern
 public
 
 type strings
-    function stringarray$[](bits:object)
+    function tostringarray$[](bits:object)
         local strarray$[]
-        if string[](bits)
+        if string(bits)
+            strarray = [string(bits)]
+        elseif string[](bits)
             strarray = string[](bits)
         elseif object[](bits)
-            local strbits$[] = object[](bits)
-            strarray = new string[strbits.length]
-            for local i%=0 until strbits.length
-                strarray[i] = strbits[i].tostring()
+            local obits:object[] = object[](bits)
+            strarray = new string[obits.length]
+            for local i%=0 until obits.length
+                strarray[i] = obits[i].tostring()
             next
         else
             local enum:Enumerator = Enumerator.get(bits)
-            strarray = new string[enum.count()]
-            local i%=0
-            for local value:object = eachin enum
-                strarray[i] = value.tostring()
-                i :+ 1
-            next
+            if enum
+                strarray = new string[enum.count()]
+                local i%=0
+                for local value:object = eachin enum
+                    strarray[i] = value.tostring()
+                    i :+ 1
+                next
+            else
+                strarray = [bits.tostring()]
+            endif
         endif
         return strarray
     end function
-    
-    function join$(delim$, bits:object)
-        return bbStringJoin(delim, stringarray(bits))
+    function tostringarrayarray$[][](bits:object)
+        local strarrayarray$[][]
+        if string[][](bits)
+            strarrayarray = string[][](bits)
+        elseif object[][](bits)
+            local obits:object[][] = object[][](bits)
+            strarrayarray = new string[][obits.length]
+            for local i%=0 until obits.length
+                strarrayarray[i] = tostringarray(obits[i])
+            next
+        else
+            local enum:Enumerator = Enumerator.get(bits)
+            strarrayarray = new string[][enum.count()]
+            local i%=0
+            for local value:object = eachin enum
+                strarrayarray[i] = tostringarray(value)
+                i :+ 1
+            next
+        endif
+        return strarrayarray
     end function
-    function joinsub$(delim$, bits:object, low%, up%)
-        return bbStringJoinSub(delim, stringarray(bits), low, up)
+    
+    function join$(delim:object, bits:object)
+        local strbits$[] = tostringarray(bits)
+        return machStringJoinSub(delim.tostring(), strbits, 0, strbits.length)
+    end function
+    function joinsub$(delim:object, bits:object, low%, up%)
+        return machStringJoinSub(delim.tostring(), tostringarray(bits), low, up)
+    end function
+    
+    function concat$(bits:object)
+        local strbits$[] = tostringarray(bits)
+        return machStringConcat(strbits, 0, strbits.length)
+    end function
+    function concatsub$(bits:object, low%, up%)
+        return machStringConcat(tostringarray(bits), low, up)
+    end function
+    
+    function interleave$(bits:object, start%=0, inc%=1)
+        local strbits$[][] = tostringarrayarray(bits)
+        return machStringInterleave(strbits, start, inc)
+    end function
+    
+    ''' Formats a string using a funny syntax because blitz is weird.
+    ''' When the format string contains %s it's replaced with the corresponsing member of subs.
+    ''' The first %s is replaced with the first sub, the second %s with the second, and on.
+    ''' When the format string contains %X where X is some digit it's replaced with the sub at
+    ''' index X. When the format string contains %[X] where X is some decimal number of any
+    ''' length it's replaced with the sub at index X. %% becomes %.
+    ''' Note that %d, %f, and other tokens that would normally have meaning someplace like this
+    ''' are not recognized.
+    ''' When a string would be formatted using a sub at an index that's out-of-bounds, the
+    ''' function will silently fail and the token will not be replaced.
+    const FORMAT_TOKEN% = asc("%")
+    function format$(str:object, sub:object, formattoken%=FORMAT_TOKEN)
+        return machStringFormat(str.tostring(), tostringarray(sub), formattoken)
     end function
 end type
+
--- strings.mod/strings.c
+++ strings.mod/strings.c
@@ -1,12 +1,99 @@
 #include <brl.mod/blitz.mod/blitz_string.h>
 #include <brl.mod/blitz.mod/blitz_array.h>
 
-// Mostly the same as the bbStringJoin function in blitz_string, except it also accepts a lower and upper bound as arguments
-BBString *bbStringJoinSub( BBString *delimiter, BBArray *bitsarray, int lower, int upper ){
+BBString *machStringFormat( BBString *str, BBArray *bitsarray, BBChar tokenchar ){
+    if( bitsarray==&bbEmptyArray ){
+        return &bbEmptyString;
+    }else{
+        int i, j, k;
+        int ontoken = 0;
+        int sindex = 0;
+        int size = str->length;
+        int numbits = bitsarray->scales[0];
+        BBString **bits = (BBString**)BBARRAYDATA( bitsarray, 1 );
+        
+        // determine a maximum number of tokens that will need to be accounted for then allocate memory for them
+        int tokenalloc = 0; i = 0; while( i < str->length ){ tokenalloc += (str->buf[i++] == tokenchar); }
+        int *tokenposition = malloc( sizeof(int)*tokenalloc );
+        int *tokenlength = malloc( sizeof(int)*tokenalloc );
+        int *tokenindex = malloc( sizeof(int)*tokenalloc );
+        
+        // find positions of tokens, their lengths, and the indexes of the bits they refer to
+        i = 0; while( i < str->length ){
+            BBChar ch = str->buf[i];
+            if( (ch == tokenchar) && ((i+1) < str->length) ){
+                ch = str->buf[i+1]; tokenposition[ ontoken ] = i; tokenlength[ ontoken ] = 2;
+                
+                if( ch == tokenchar ){              // %%
+                    tokenindex[ ontoken ] = -1;
+                }else if( ch == 115 || ch == 83 ){  // %s
+                    tokenindex[ ontoken ] = sindex++;
+                }else if( ch >= 48 && ch <= 57 ){   // %0, %1, %9, etc.
+                    tokenindex[ ontoken ] = ch - 48;
+                }else if( ch == 91 ){               // %[10], %[42], etc.
+                    tokenindex[ ontoken ] = 0; j = ++i;
+                    while( j < str->length && str->buf[j] != 93 ){ j++; } // find the closing brace
+                    if( j < str->length && j-i > 1 ){
+                        tokenlength[ ontoken ] += j-i;
+                        int digit = 1; k = i; i = j; while( --j>k ){ tokenindex[ ontoken ] += digit * (str->buf[j] - 48); digit *= 10; } // turn e.g. "512" into (int)512.
+                    }else{
+                        i = j; continue; // skip things like "%[...", "%[]"
+                    }
+                }
+                
+                if( tokenindex[ ontoken ] >= 0 && tokenindex[ ontoken ] < numbits ){ // ignore token if its index is out of bounds
+                    size += bits[ tokenindex[ ontoken ] ]->length - tokenlength[ ontoken ]; ontoken++;
+                }
+            }
+            i++;
+        }
+        
+        // build the resulting string
+        BBString *result;
+        if( ontoken == 0 ){ // found no valid tokens?
+            result = str;
+        }else if( size <= 0 ){ // empty string?
+            result = &bbEmptyString;
+        }else{ // all is well, continue as normal
+            result = bbStringNew( size );
+            BBString *bit;
+            int l;
+            i = 0; // index of result
+            j = 0; // index of str
+            k = 0; // current token
+            while( k < ontoken ){
+                l = tokenposition[k] - j; // length of section from str
+                memcpy( result->buf + i, str->buf + j, l*sizeof(BBChar) );
+                i += l; j += l + tokenlength[k];
+                if( tokenindex[k] >= 0 ){
+                    bit = bits[ tokenindex[k] ];
+                    memcpy( result->buf + i, bit->buf, bit->length*sizeof(BBChar) );
+                    i += bit->length;
+                }else{
+                    result->buf[i++] = tokenchar;
+                }
+                k++;
+            }
+            memcpy( result->buf + i, str->buf + j, (str->length - j)*sizeof(BBChar) );
+            return result;
+        }
+        
+        // cleanup
+        free( tokenposition );
+        free( tokenlength );
+        free( tokenindex );
+        
+        // all done!
+        return result;
+    }
+}
+
+// Mostly the same as the bbStringJoin function in blitz_string.c, except it also accepts a lower and upper bound as arguments
+BBString *machStringJoinSub( BBString *delimiter, BBArray *bitsarray, int lower, int upper ){
     if( bitsarray==&bbEmptyArray ){
         return &bbEmptyString;
     }else{        
-        BBString **bits = (BBString**)BBARRAYDATA( bitsarray,1 );
+        BBString **bits = (BBString**)BBARRAYDATA( bitsarray, 1 );
         int size = 0;
         
         BBString **b; int i;
@@ -34,6 +121,105 @@ BBString *bbStringJoinSub( BBString *delimiter, BBArray *bitsarray, int lower, i
             strbuffer += bit->length;
         }
         
+        return str;
+    }
+}
+
+// Same as bbStringJoinSub but without caring about a delimiter
+BBString *machStringConcat( BBArray *bitsarray, int lower, int upper ){
+    if( bitsarray==&bbEmptyArray ){
+        return &bbEmptyString;
+    }else{        
+        BBString **bits = (BBString**)BBARRAYDATA( bitsarray, 1 );
+        int size = 0;
+        
+        BBString **b; int i;
+        
+        b = bits;
+        for( i=lower; i<upper; ++i ){
+            BBString *bit=*b++;
+            size += bit->length;
+        }
+        
+        BBString *str = bbStringNew( size );
+        BBChar *strbuffer = str->buf;
+        
+        b = bits;
+        for( i=lower; i<upper; ++i ){
+            BBString *bit = *b++;
+            memcpy( strbuffer, bit->buf, bit->length*sizeof(BBChar) );
+            strbuffer += bit->length;
+        }
+        
+        return str;
+    }
+}
+
+BBString *machStringInterleave( BBArray *superarray, int start, int step ){
+    if( superarray==&bbEmptyArray ){
+        return &bbEmptyString;
+    }else{
+        int i, j;
+        int size = 0;
+        int totalbits = 0;
+        int onbit = 0;
+        int usedbits;
+        int numbits;
+        int numbitsarrays = superarray->scales[0];
+        int *onsubbit = malloc( sizeof(int)*numbitsarrays );
+        BBArray **bitsarrays = (BBArray**)BBARRAYDATA( superarray, 1 );
+        BBArray **b = bitsarrays;
+        
+        for( i=0; i<numbitsarrays; ++i ){
+            onsubbit[i] = 0;
+            BBArray *bitsarray = *b++;
+            numbits = bitsarray->scales[0];
+            totalbits += numbits;
+        }
+        
+        i = start;
+        while(onbit < totalbits){
+            j = 0;
+            while( onsubbit[i] >= bitsarrays[i]->scales[0] ){
+                if( j > numbitsarrays ){ goto counted; }
+                i = (i + step) % numbitsarrays;
+                j++;
+            }
+            
+            BBString **bits = (BBString**)BBARRAYDATA( bitsarrays[i], 1 );
+            BBString *bit = bits[ onsubbit[i] ];
+            size += bit->length;
+            
+            onsubbit[i]++;
+            i = (i + step) % numbitsarrays;
+            onbit++;
+        }
+        
+        counted:
+        usedbits = onbit;
+        onbit = 0;
+        for( i=0; i<numbitsarrays; ++i ){ onsubbit[i] = 0; }
+        
+        BBString *str = bbStringNew( size );
+        BBChar *strbuffer = str->buf;
+        
+        i = start;
+        while(onbit < usedbits){
+            while( onsubbit[i] >= bitsarrays[i]->scales[0] ){
+                i = (i + step) % numbitsarrays;
+            }
+            
+            BBString **bits = (BBString**)BBARRAYDATA( bitsarrays[i], 1 );
+            BBString *bit = bits[ onsubbit[i] ];
+            memcpy( strbuffer, bit->buf, bit->length*sizeof(BBChar) );
+            strbuffer += bit->length;
+            
+            onsubbit[i]++;
+            i = (i + step) % numbitsarrays;
+            onbit++;
+        }
+        
+        free( onsubbit );
         return str;
     }
 }
